72e6f45a05753393be239b6d7c012f39c65d347f
Joses W. Ho <joseshowh@gmail.com>
2019-02-22 13:39:42 +0800

    add effect size tests for mean and median diff

--- dabest/tests/test_effsizes.py
+++ dabest/tests/test_effsizes.py
@@ -1,18 +1,26 @@
 # coding: utf-8
 #! /usr/bin/env python
 
+
+ # This filters out an innocuous warning when pandas is imported,
+# # but the version has not been compiled against the newest numpy.
+# import warnings
+# warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+
+
+
 import pytest
 import sys
 import numpy as np
 import scipy as sp
 
-# This filters out an innocuous warning when pandas is imported,
-# but the version has not been compiled against the newest numpy.
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-
 import pandas as pd
-from .._stats_tools import effsize
+from ..stats_tools import effsize
+
+
+
+
+
 
 # Data for tests.
 # See Cumming, G. Understanding the New Statistics:
@@ -22,10 +30,14 @@ wb = {"control": [34, 54, 33, 44, 45, 53, 37, 26, 38, 58],
       "expt":    [66, 38, 35, 55, 48, 39, 65, 32, 57, 41]}
 wellbeing = pd.DataFrame(wb)
 
+
+
 # from Cumming 2012 Table 11.2 Page 291
 paired_wb = {"pre":     [43, 28, 54, 36, 31, 48, 50, 69, 29, 40],
              "post":    [51, 33, 58, 42, 39, 45, 54, 68, 35, 44]}
 paired_wellbeing = pd.DataFrame(paired_wb)
+
+
 
 # Data from Hogarty and Kromrey (1999)
 # Kromrey, Jeffrey D., and Kristine Y. Hogarty. 1998.
@@ -36,6 +48,8 @@ paired_wellbeing = pd.DataFrame(paired_wb)
 likert_control   = [1, 1, 2, 2, 2, 3, 3, 3, 4, 5]
 likert_treatment = [1, 2, 3, 4, 4, 5]
 
+
+
 # Data from Cliff (1993)
 # Cliff, Norman. 1993. “Dominance Statistics: Ordinal Analyses to Answer
 # Ordinal Questions.”
@@ -44,39 +58,83 @@ a_scores = [6, 7, 9, 10]
 b_scores = [1, 3, 4, 7, 8]
 
 
+
+
+
+
+def test_mean_diff_unpaired():
+    import numpy as np
+    mean_diff = effsize.func_difference(wellbeing.control, wellbeing.expt,
+                                        np.mean, is_paired=False)
+    assert mean_diff == pytest.approx(5.4)
+
+
+
+def test_median_diff_unpaired():
+    from numpy import median as npmedian
+    median_diff = effsize.func_difference(wellbeing.control, wellbeing.expt,
+                                        npmedian, is_paired=False)
+    assert median_diff == pytest.approx(3.5)
+
+
+
+def test_mean_diff_paired():
+    from numpy import mean as npmean
+    mean_diff = effsize.func_difference(paired_wellbeing.pre,
+                                        paired_wellbeing.post,
+                                        npmean, is_paired=True)
+    assert mean_diff == pytest.approx(4.10)
+
+
+
+def test_median_diff_paired():
+    from numpy import median as npmedian
+    median_diff = effsize.func_difference(paired_wellbeing.pre,
+                                          paired_wellbeing.post,
+                                          npmedian, is_paired=True)
+    assert median_diff == pytest.approx(4.5)
+
+
+
+
+
+
 def test_cohens_d_unpaired():
-    print("Testing Unpaired Cohen's d.")
-    cohens_d = effsize.cohens_d(wellbeing.control, wellbeing.expt, paired=False)
-    assert cohens_d == pytest.approx(0.47, rel=0.01)
+    import numpy as np
+    cohens_d = effsize.cohens_d(wellbeing.control, wellbeing.expt,
+                                is_paired=False)
+    assert np.round(cohens_d, 2) == pytest.approx(0.47)
 
 
 
 def test_hedges_g_unpaired():
-    print("Testing Unpaired Hedges' g.")
-    cohens_d = effsize.hedges_g(wellbeing.control, wellbeing.expt, paired=False)
-    assert cohens_d == pytest.approx(0.45, rel=0.01)
+    import numpy as np
+    hedges_g = effsize.hedges_g(wellbeing.control, wellbeing.expt,
+                                is_paired=False)
+    assert np.round(hedges_g, 2) == pytest.approx(0.45)
 
 
 
 def test_cohens_d_paired():
-    print("Testing Paired Cohen's d.")
+    import numpy as np
     cohens_d = effsize.cohens_d(paired_wellbeing.pre, paired_wellbeing.post,
-                                paired=True)
-    assert cohens_d == pytest.approx(0.34, rel=0.05)
+                                is_paired=True)
+    assert np.round(cohens_d, 2) == pytest.approx(0.34)
 
 
 
 def test_hedges_g_paired():
-    print("Testing Paired Hedges' g.")
-    cohens_d = effsize.hedges_g(paired_wellbeing.pre, paired_wellbeing.post,
-                                paired=True)
-    assert cohens_d == pytest.approx(0.32, rel=0.05)
+    import numpy as np
+    hedges_g = effsize.hedges_g(paired_wellbeing.pre, paired_wellbeing.post,
+                                is_paired=True)
+    assert np.round(hedges_g, 2) == pytest.approx(0.33)
+
+
+
 
 
 
 def test_cliffs_delta():
-    print("Testing Cliff's delta.")
-
     likert_delta = effsize.cliffs_delta(likert_treatment, likert_control)
     assert likert_delta == pytest.approx(-0.25)
 
