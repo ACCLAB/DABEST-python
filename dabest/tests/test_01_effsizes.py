a35d20d47f1c749dcbe94a8f626e06f58096e7c7
Joses W. Ho <joseshowh@gmail.com>
2019-02-26 18:06:42 +0800

    refactor tests

---
+++ dabest/tests/test_01_effsizes.py
@@ -0,0 +1,141 @@
+# coding: utf-8
+#! /usr/bin/env python
+
+
+ # This filters out an innocuous warning when pandas is imported,
+# # but the version has not been compiled against the newest numpy.
+# import warnings
+# warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+
+
+
+import sys
+import numpy as np
+import scipy as sp
+import pytest
+import pandas as pd
+from ..stats_tools import effsize
+
+
+
+
+
+
+# Data for tests.
+# See Cumming, G. Understanding the New Statistics:
+# Effect Sizes, Confidence Intervals, and Meta-Analysis. Routledge, 2012,
+# from Cumming 2012 Table 11.1 Pg 287.
+wb = {"control": [34, 54, 33, 44, 45, 53, 37, 26, 38, 58],
+      "expt":    [66, 38, 35, 55, 48, 39, 65, 32, 57, 41]}
+wellbeing = pd.DataFrame(wb)
+
+
+
+# from Cumming 2012 Table 11.2 Page 291
+paired_wb = {"pre":     [43, 28, 54, 36, 31, 48, 50, 69, 29, 40],
+             "post":    [51, 33, 58, 42, 39, 45, 54, 68, 35, 44]}
+paired_wellbeing = pd.DataFrame(paired_wb)
+
+
+
+# Data from Hogarty and Kromrey (1999)
+# Kromrey, Jeffrey D., and Kristine Y. Hogarty. 1998.
+# “Analysis Options for Testing Group Differences on Ordered Categorical
+# Variables: An Empirical Investigation of Type I Error Control
+# Statistical Power.”
+# Multiple Linear Regression Viewpoints 25 (1): 70 - 82.
+likert_control   = [1, 1, 2, 2, 2, 3, 3, 3, 4, 5]
+likert_treatment = [1, 2, 3, 4, 4, 5]
+
+
+
+# Data from Cliff (1993)
+# Cliff, Norman. 1993. “Dominance Statistics: Ordinal Analyses to Answer
+# Ordinal Questions.”
+# Psychological Bulletin 114 (3): 494–509.
+a_scores = [6, 7, 9, 10]
+b_scores = [1, 3, 4, 7, 8]
+
+
+
+
+
+
+def test_mean_diff_unpaired():
+    import numpy as np
+    mean_diff = effsize.func_difference(wellbeing.control, wellbeing.expt,
+                                        np.mean, is_paired=False)
+    assert mean_diff == pytest.approx(5.4)
+
+
+
+def test_median_diff_unpaired():
+    from numpy import median as npmedian
+    median_diff = effsize.func_difference(wellbeing.control, wellbeing.expt,
+                                        npmedian, is_paired=False)
+    assert median_diff == pytest.approx(3.5)
+
+
+
+def test_mean_diff_paired():
+    from numpy import mean as npmean
+    mean_diff = effsize.func_difference(paired_wellbeing.pre,
+                                        paired_wellbeing.post,
+                                        npmean, is_paired=True)
+    assert mean_diff == pytest.approx(4.10)
+
+
+
+def test_median_diff_paired():
+    from numpy import median as npmedian
+    median_diff = effsize.func_difference(paired_wellbeing.pre,
+                                          paired_wellbeing.post,
+                                          npmedian, is_paired=True)
+    assert median_diff == pytest.approx(4.5)
+
+
+
+
+
+
+def test_cohens_d_unpaired():
+    import numpy as np
+    cohens_d = effsize.cohens_d(wellbeing.control, wellbeing.expt,
+                                is_paired=False)
+    assert np.round(cohens_d, 2) == pytest.approx(0.47)
+
+
+
+def test_hedges_g_unpaired():
+    import numpy as np
+    hedges_g = effsize.hedges_g(wellbeing.control, wellbeing.expt,
+                                is_paired=False)
+    assert np.round(hedges_g, 2) == pytest.approx(0.45)
+
+
+
+def test_cohens_d_paired():
+    import numpy as np
+    cohens_d = effsize.cohens_d(paired_wellbeing.pre, paired_wellbeing.post,
+                                is_paired=True)
+    assert np.round(cohens_d, 2) == pytest.approx(0.34)
+
+
+
+def test_hedges_g_paired():
+    import numpy as np
+    hedges_g = effsize.hedges_g(paired_wellbeing.pre, paired_wellbeing.post,
+                                is_paired=True)
+    assert np.round(hedges_g, 2) == pytest.approx(0.33)
+
+
+
+
+
+
+def test_cliffs_delta():
+    likert_delta = effsize.cliffs_delta(likert_treatment, likert_control)
+    assert likert_delta == pytest.approx(-0.25)
+
+    scores_delta = effsize.cliffs_delta(b_scores, a_scores)
+    assert scores_delta == pytest.approx(0.65)
---
+++ dabest/tests/test_02_plotting.py
@@ -0,0 +1,135 @@
+# #! /usr/bin/env python
+
+# Load Libraries
+
+import pytest
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use('Agg')
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import seaborn as sns
+from ..api import load
+from .utils import create_dummy_dataset, get_swarm_yspans
+
+
+
+def test_gardner_altman_unpaired():
+
+    base_mean = np.random.randint(10, 101)
+    seed, ptp, df = create_dummy_dataset(base_mean=base_mean)
+    print('\nSeed = {}; base mean = {}'.format(seed, base_mean))
+
+    for c in df.columns[1:-1]:
+        print('{}...'.format(c))
+
+        # Create Gardner-Altman plot.
+        rand_swarm_ylim = (np.random.uniform(base_mean-10, base_mean, 1),
+                           np.random.uniform(base_mean, base_mean+10, 1))
+        two_group_unpaired = load(df, idx=(df.columns[0], c))
+        f1 = two_group_unpaired.mean_diff.plot(swarm_ylim=rand_swarm_ylim,
+                                               swarm_label="Raw swarmplot...",
+                                               contrast_label="Contrast!")
+
+        rawswarm_axes = f1.axes[0]
+        contrast_axes = f1.axes[1]
+
+        # Check ylims match the desired ones.
+        assert rawswarm_axes.get_ylim()[0] == pytest.approx(rand_swarm_ylim[0])
+        assert rawswarm_axes.get_ylim()[1] == pytest.approx(rand_swarm_ylim[1])
+
+
+        # Check each swarmplot group matches canonical seaborn swarmplot.
+        _, swarmplt = plt.subplots(1)
+        swarmplt.set_ylim(rand_swarm_ylim)
+        sns.swarmplot(data=df[[df.columns[0], c]], ax=swarmplt)
+        sns_yspans = []
+        for coll in swarmplt.collections:
+            sns_yspans.append(get_swarm_yspans(coll))
+        dabest_yspans = [get_swarm_yspans(coll)
+                        for coll in rawswarm_axes.collections]
+        for j, span in enumerate(sns_yspans):
+            assert span == pytest.approx(dabest_yspans[j])
+
+        # Check xtick labels.
+        swarm_xticks = [a.get_text() for a in rawswarm_axes.get_xticklabels()]
+        assert swarm_xticks[0] == "{}\nN = 30".format(df.columns[0])
+        assert swarm_xticks[1] == "{}\nN = 30".format(c)
+
+        contrast_xticks = [a.get_text() for a in contrast_axes.get_xticklabels()]
+        assert contrast_xticks[1] == "{}\nminus\n{}".format(c, df.columns[0])
+
+        # Check ylabels.
+        assert rawswarm_axes.get_ylabel() == "Raw swarmplot..."
+        assert contrast_axes.get_ylabel() == "Contrast!"
+
+
+
+
+
+def test_cummings_unpaired():
+    base_mean = np.random.randint(10, 101)
+    seed, ptp, df = create_dummy_dataset(base_mean=base_mean, expt_groups=7)
+    print('\nSeed = {}; base mean = {}'.format(seed, base_mean))
+
+    IDX = (('0','5'), ('3','2'), ('4', '1', '6'))
+    multi_2group_unpaired = load(df, idx=IDX)
+
+    rand_swarm_ylim = (np.random.uniform(base_mean-10, base_mean, 1),
+                       np.random.uniform(base_mean, base_mean+10, 1))
+    rand_contrast_ylim = (-base_mean/3, base_mean/3)
+
+    f1 = multi_2group_unpaired.mean_diff.plot(swarm_ylim=rand_swarm_ylim,
+                                              contrast_ylim=rand_contrast_ylim,
+                                              swarm_label="Raw swarmplot!",
+                                              contrast_label="Contrast...")
+
+    rawswarm_axes = f1.axes[0]
+    contrast_axes = f1.axes[1]
+
+    # Check ylims match the desired ones.
+    assert rawswarm_axes.get_ylim()[0] == pytest.approx(rand_swarm_ylim[0])
+    assert rawswarm_axes.get_ylim()[1] == pytest.approx(rand_swarm_ylim[1])
+    assert contrast_axes.get_ylim()[0] == pytest.approx(rand_contrast_ylim[0])
+    assert contrast_axes.get_ylim()[1] == pytest.approx(rand_contrast_ylim[1])
+
+    # Check xtick labels.
+    idx_flat = [g for t in IDX for g in t]
+    swarm_xticks = [a.get_text() for a in rawswarm_axes.get_xticklabels()]
+    for j, xtick in enumerate(swarm_xticks):
+        assert xtick == "{}\nN = 30".format(idx_flat[j])
+
+    contrast_xticks = [a.get_text() for a in contrast_axes.get_xticklabels()]
+    assert contrast_xticks[1] == "5\nminus\n0"
+    assert contrast_xticks[3] == "2\nminus\n3"
+    assert contrast_xticks[5] == "1\nminus\n4"
+    assert contrast_xticks[6] == "6\nminus\n4"
+
+    # Check ylabels.
+    assert rawswarm_axes.get_ylabel() == "Raw swarmplot!"
+    assert contrast_axes.get_ylabel() == "Contrast..."
+
+
+
+
+
+def test_gardner_altman_paired():
+    base_mean = np.random.randint(10, 101)
+    seed, ptp, df = create_dummy_dataset(base_mean=base_mean)
+
+    two_group_unpaired = load(df, idx=(df.columns[0], c))
+
+    rand_swarm_ylim = (np.random.uniform(base_mean-10, base_mean, 1),
+                       np.random.uniform(base_mean, base_mean+10, 1))
+                       
+    f1 = two_group_unpaired.mean_diff.plot(swarm_ylim=rand_swarm_ylim,
+                                           swarm_label="Raw swarmplot...",
+                                           contrast_label="Contrast!")
+
+    rawswarm_axes = f1.axes[0]
+    contrast_axes = f1.axes[1]
+
+    # assert df['0'].tolist() == [l.get_ydata()[0] for l in rawswarm_axes.lines]
+    # assert df['1'].tolist() == [l.get_ydata()[1] for l in rawswarm_axes.lines]
