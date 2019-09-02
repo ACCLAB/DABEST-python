75cc1e18c3682cb57b48db60a7081b7b9f28b390
Joses W. Ho <joseshowh@gmail.com>
2019-09-02 16:39:10 +0800

    fix bug in computing 95CI for paired effect sizes

--- dabest/_stats_tools/confint_1group.py
+++ dabest/_stats_tools/confint_1group.py
@@ -2,15 +2,35 @@
 # -*-coding: utf-8 -*-
 # Author: Joses Ho
 # Email : joseshowh@gmail.com
+"""
+A range of functions to compute bootstraps for a single sample.
+"""
 
+def create_bootstrap_indexes(array, resamples=5000, random_seed=12345):
+    """Given an array-like, returns a generator of bootstrap indexes
+    to be used for resampling.
+    """
+    import numpy as np
+    
+    # Set seed.
+    np.random.seed(random_seed)
+    
+    indexes = range(0, len(array))
 
+    out = (np.random.choice(indexes, len(indexes), replace=True)
+            for i in range(0, resamples))
+    
+    # Reset seed
+    np.random.seed()
+    
+    return out
 
 def compute_1group_jackknife(x, func, *args, **kwargs):
     """
     Returns the jackknife bootstraps for func(x).
     """
-    from . import confint_meandiff as ci_md
-    jackknives = [i for i in ci_md.create_jackknife_indexes(x)]
+    from . import confint_2group_diff as ci_2g
+    jackknives = [i for i in ci_2g.create_jackknife_indexes(x)]
     out = [func(x[j], *args, **kwargs) for j in jackknives]
     del jackknives # memory management.
     return out
@@ -18,19 +38,18 @@ def compute_1group_jackknife(x, func, *args, **kwargs):
 
 
 def compute_1group_acceleration(jack_dist):
-    from . import confint_meandiff as ci_md
-    return ci_md._calc_accel(jack_dist)
+    from . import confint_2group_diff as ci_2g
+    return ci_2g._calc_accel(jack_dist)
 
 
 
 def compute_1group_bootstraps(x, func, resamples=5000, random_seed=12345,
                              *args, **kwargs):
     """Bootstraps func(x), with the number of specified resamples."""
-    from . import confint_meandiff as ci_md
 
     # Create bootstrap indexes.
-    bs_index_kwargs = {'resamples': resamples, 'random_seed': random_seed}
-    boot_indexes = ci_md._create_bootstrap_indexes(len(x), **bs_index_kwargs)
+    boot_indexes = create_bootstrap_indexes(x, resamples=resamples,
+                                            random_seed=random_seed)
 
     out = [func(x[b], *args, **kwargs) for b in boot_indexes]
     del boot_indexes
@@ -101,10 +120,12 @@ def summary_ci_1group(x, func, resamples=5000, alpha=0.05, random_seed=12345,
     from . import confint_2group_diff as ci2g
     from numpy import sort as npsort
 
-    boots = compute_1group_bootstraps(x, func)
+    boots = compute_1group_bootstraps(x, func, resamples=resamples,
+                                      random_seed=random_seed,
+                                      *args, **kwargs)
     bias = compute_1group_bias_correction(x, boots, func)
 
-    jk = compute_1group_jackknife(x, func)
+    jk = compute_1group_jackknife(x, func, *args, **kwargs)
     accel = compute_1group_acceleration(jk)
     del jk
 
