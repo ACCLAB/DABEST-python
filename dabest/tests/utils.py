a8a953ddd18ad52e47e8f2ab7a6a2d3c34a54812
Joses W. Ho <joseshowh@gmail.com>
2019-10-07 18:13:26 +0800

    use pytest-mpl plugin for plot testing

--- dabest/tests/utils.py
+++ dabest/tests/utils.py
@@ -1,45 +1,42 @@
-def create_dummy_dataset(seed=None, n=30, base_mean=0,
-                         plus_minus=5, expt_groups=7,
-                         scale_means=1., scale_std=1.):
-    """
-    Creates a dummy dataset for plotting.
-    Returns the seed used to generate the random numbers,
-    the maximum possible difference between mean differences,
-    and the dataset itself.
-    """
+def create_demo_dataset(seed=9999, N=20):
+    
     import numpy as np
-    import scipy as sp
     import pandas as pd
+    from scipy.stats import norm # Used in generation of populations.
 
-    # Set a random seed.
-    if seed is None:
-        random_seed = np.random.randint(low=1, high=1000, size=1)[0]
-    else:
-        if isinstance(seed, int):
-            random_seed = seed
-        else:
-            raise TypeError('{} is not an integer.'.format(seed))
+    np.random.seed(9999) # Fix the seed so the results are replicable.
+    # pop_size = 10000 # Size of each population.
 
-    # Generate a set of random means
-    np.random.seed(random_seed)
-    MEANS = np.repeat(base_mean, expt_groups) + \
-            np.random.uniform(base_mean-plus_minus, base_mean+plus_minus,
-                              expt_groups) * scale_means
-    SCALES = np.random.random(size=expt_groups) * scale_std
+    # Create samples
+    c1 = norm.rvs(loc=3, scale=0.4, size=N)
+    c2 = norm.rvs(loc=3.5, scale=0.75, size=N)
+    c3 = norm.rvs(loc=3.25, scale=0.4, size=N)
 
-    max_mean_diff = np.ptp(MEANS)
+    t1 = norm.rvs(loc=3.5, scale=0.5, size=N)
+    t2 = norm.rvs(loc=2.5, scale=0.6, size=N)
+    t3 = norm.rvs(loc=3, scale=0.75, size=N)
+    t4 = norm.rvs(loc=3.5, scale=0.75, size=N)
+    t5 = norm.rvs(loc=3.25, scale=0.4, size=N)
+    t6 = norm.rvs(loc=3.25, scale=0.4, size=N)
 
-    dataset = list()
-    for i, m in enumerate(MEANS):
-        pop = sp.stats.norm.rvs(loc=m, scale=SCALES[i], size=10000)
-        sample = np.random.choice(pop, size=n, replace=False)
-        dataset.append(sample)
 
-    df = pd.DataFrame(dataset).T
-    df["idcol"] = pd.Series(range(1, n+1))
-    df.columns = [str(c) for c in df.columns]
+    # Add a `gender` column for coloring the data.
+    females = np.repeat('Female', N/2).tolist()
+    males = np.repeat('Male', N/2).tolist()
+    gender = females + males
 
-    return random_seed, max_mean_diff, df
+    # Add an `id` column for paired data plotting. 
+    id_col = pd.Series(range(1, N+1))
+
+    # Combine samples and gender into a DataFrame.
+    df = pd.DataFrame({'Control 1' : c1,     'Test 1' : t1,
+                       'Control 2' : c2,     'Test 2' : t2,
+                       'Control 3' : c3,     'Test 3' : t3,
+                       'Test 4'    : t4,     'Test 5' : t5, 'Test 6' : t6,
+                       'Gender'    : gender, 'ID'  : id_col
+                      })
+                      
+    return df
 
 
 
@@ -58,3 +55,48 @@ def get_swarm_yspans(coll, round_result=False, decimals=12):
             return y.min(), y.max()
     except ValueError:
         return None
+        
+        
+        
+# def create_dummy_dataset(seed=None, n=30, base_mean=0,
+#                          plus_minus=5, expt_groups=7,
+#                          scale_means=1., scale_std=1.):
+#     """
+#     Creates a dummy dataset for plotting.
+#     Returns the seed used to generate the random numbers,
+#     the maximum possible difference between mean differences,
+#     and the dataset itself.
+#     """
+#     import numpy as np
+#     import scipy as sp
+#     import pandas as pd
+# 
+#     # Set a random seed.
+#     if seed is None:
+#         random_seed = np.random.randint(low=1, high=1000, size=1)[0]
+#     else:
+#         if isinstance(seed, int):
+#             random_seed = seed
+#         else:
+#             raise TypeError('{} is not an integer.'.format(seed))
+# 
+#     # Generate a set of random means
+#     np.random.seed(random_seed)
+#     MEANS = np.repeat(base_mean, expt_groups) + \
+#             np.random.uniform(base_mean-plus_minus, base_mean+plus_minus,
+#                               expt_groups) * scale_means
+#     SCALES = np.random.random(size=expt_groups) * scale_std
+# 
+#     max_mean_diff = np.ptp(MEANS)
+# 
+#     dataset = list()
+#     for i, m in enumerate(MEANS):
+#         pop = sp.stats.norm.rvs(loc=m, scale=SCALES[i], size=10000)
+#         sample = np.random.choice(pop, size=n, replace=False)
+#         dataset.append(sample)
+# 
+#     df = pd.DataFrame(dataset).T
+#     df["idcol"] = pd.Series(range(1, n+1))
+#     df.columns = [str(c) for c in df.columns]
+# 
+#     return random_seed, max_mean_diff, df
