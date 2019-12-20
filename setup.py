0391fee8337bc74ec582bc51116b046ca22e1ceb
Joses W. Ho <joseshowh@gmail.com>
2019-12-20 14:50:33 +0800

    fix required package and python versions

--- setup.py
+++ setup.py
@@ -41,13 +41,13 @@ if __name__ == "__main__":
         install_requires=[
             'numpy~=1.15',
             'scipy~=1.2',
-            'pandas==0.24',
+            'pandas~=0.25,!=0.25.2',
             
             'matplotlib~=3.0',
             'seaborn~=0.9'
         ],
-        extras_require={'dev': ['pytest==5.2', 'pytest-mpl==0.10']},
-        python_requires='>=3.5',
+        extras_require={'dev': ['pytest~=5.2', 'pytest-mpl~=0.10']},
+        python_requires='~=3.5',
         url='https://acclab.github.io/DABEST-python-docs',
         download_url='https://www.github.com/ACCLAB/DABEST-python',
         license='BSD 3-clause Clear License'
