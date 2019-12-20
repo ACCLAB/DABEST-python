08d45380a110c7cc940641c2ce9c040a3ea29768
Joses W. Ho <joseshowh@gmail.com>
2019-12-20 14:22:56 +0800

    bump v0.2.8

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         author_email='joseshowh@gmail.com',
         maintainer='Joses W. Ho',
         maintainer_email='joseshowh@gmail.com',
-        version='0.2.7',
+        version='0.2.8',
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
