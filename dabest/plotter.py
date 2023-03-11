d3471f980c30ceb43a6674c4e681e453ed968799
Zhang Rou <e0950086@u.nus.edu>
2023-03-11 22:02:47 +0800

    run nbdev_export

--- dabest/plotter.py
+++ dabest/plotter.py
@@ -40,6 +40,8 @@ def EffectSizeDataFramePlotter(EffectSizeDataFrame, **plot_kwargs):
     import seaborn as sns
     import matplotlib.pyplot as plt
     import pandas as pd
+    import warnings
+    warnings.filterwarnings('ignore', 'This figure includes Axes that are not compatible with tight_layout')
 
     from .misc_tools import merge_two_dicts
     from .plot_tools import halfviolin, get_swarm_spans, gapped_lines,proportion_error_bar
