e9e72a036721399013a0f5f214d7d5f1fac3120b
maiyishan <maiyishan@gmail.com>
2023-03-19 16:13:20 +0800

    Mini-meta docs change in classes.py
    Fixes code in API page for mini-meta

--- dabest/_classes.py
+++ dabest/_classes.py
@@ -1316,17 +1316,17 @@ class MiniMetaDelta(object):
     >>> from scipy.stats import norm
     >>> import pandas as pd
     >>> import dabest
+    >>> Ns = 20
     >>> c1 = norm.rvs(loc=3, scale=0.4, size=Ns)
     >>> c2 = norm.rvs(loc=3.5, scale=0.75, size=Ns)
     >>> c3 = norm.rvs(loc=3.25, scale=0.4, size=Ns)
-
     >>> t1 = norm.rvs(loc=3.5, scale=0.5, size=Ns)
     >>> t2 = norm.rvs(loc=2.5, scale=0.6, size=Ns)
     >>> t3 = norm.rvs(loc=3, scale=0.75, size=Ns)
     >>> my_df   = pd.DataFrame({'Control 1' : c1,     'Test 1' : t1,
                        'Control 2' : c2,     'Test 2' : t2,
                        'Control 3' : c3,     'Test 3' : t3})
-    >>> my_dabest_object = dabest.load(df, idx=(("Control 1", "Test 1"), ("Control 2", "Test 2"), ("Control 3", "Test 3")), mini_meta=True)
+    >>> my_dabest_object = dabest.load(my_df, idx=(("Control 1", "Test 1"), ("Control 2", "Test 2"), ("Control 3", "Test 3")), mini_meta=True)
     >>> my_dabest_object.mean_diff.mini_meta_delta
 
     Notes
