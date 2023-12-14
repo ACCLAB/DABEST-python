1c852bdf359d0461298c94448548ad259d4c1759
cyberosa <arosacastillo@protonmail.com>
2023-12-14 17:23:46 +0100

    delta objects notebook

--- dabest/plot_tools.py
+++ dabest/plot_tools.py
@@ -41,28 +41,6 @@ def halfviolin(v, half='right', fill_color='k', alpha=1,
         b.set_linewidth(line_width)
 
 
-
-# def align_yaxis(ax1, v1, ax2, v2):
-#     """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
-#     # Taken from
-#     # http://stackoverflow.com/questions/7630778/
-#     # matplotlib-align-origin-of-right-axis-with-specific-left-axis-value
-#     _, y1 = ax1.transData.transform((0, v1))
-#     _, y2 = ax2.transData.transform((0, v2))
-#     inv = ax2.transData.inverted()
-#     _, dy = inv.transform((0, 0)) - inv.transform((0, y1-y2))
-#     miny, maxy = ax2.get_ylim()
-#     ax2.set_ylim(miny+dy, maxy+dy)
-#
-#
-#
-# def rotate_ticks(axes, angle=45, alignment='right'):
-#     for tick in axes.get_xticklabels():
-#         tick.set_rotation(angle)
-#         tick.set_horizontalalignment(alignment)
-
-
-
 def get_swarm_spans(coll):
     """
     Given a matplotlib Collection, will obtain the x and y spans
